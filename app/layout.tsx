002f1099982bdd9616d227bdb2a6fca29f00ee31
Markus Persson <markusperssonbiz@gmail.com>
2023-05-08 19:34:31 +0200

    Created filter modal and functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./globals.css";
 import { Nunito } from "next/font/google";
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";
+import SearchModal from "./components/modals/SearchModal";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default async function RootLayout({
       <body className={nunito.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
           <RentModal />
           <LoginModal />
           <RegisterModal />
